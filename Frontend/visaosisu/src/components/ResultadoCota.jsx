718b559ef548058abd975bc41f319979868458d3
inaciolimaf <92096585+inaciolimaf@users.noreply.github.com>
2023-07-19 15:47:10 -0300

    Concluindo o Home

--- Frontend/visaosisu/src/components/ResultadoCota.jsx
+++ Frontend/visaosisu/src/components/ResultadoCota.jsx
@@ -4,14 +4,25 @@ export default function ResultadoCota(props){
     return(
         <div>
             {props.resultados.map((resultado, index)=>{
+                notas = []
                 for (let index = 0; index < 9; index++) {
-                    notas = []
-                    console.log(resultado["Nota_Corte_"+(index+1)+"_Dia"])
+                    if (resultado["Nota_Corte_"+(index+1)+"_Dia"]!=-1) {
+                        notas.push(resultado["Nota_Corte_"+(index+1)+"_Dia"])
+                    }
                 }
                 return(
-                    <div className={styles.resultado}>
+                    <div className={styles.resultado} key={index}>
                         <p className={styles.cota}>{resultado.Cota}</p>
-                        <p className={styles.nota}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, vitae? Quam aperiam fuga qui blanditiis saepe dolorem, dolores earum doloremque odio, porro vel nesciunt accusamus eveniet? Vitae accusantium obcaecati molestias! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod id natus qui! Architecto, voluptas eligendi. Officiis assumenda aliquid, delectus earum, vitae eveniet minima fugiat repudiandae laborum necessitatibus libero nemo expedita!</p>
+                        <p className={styles.nota}>
+                            <strong>NOTAS DE CORTE:</strong>
+                            {notas.map((nota, index)=>{
+                                return (
+                                    <p><span className={styles.notaDia}>{index+1}° DIA: <strong>{nota}</strong></span></p>
+                                )
+                            })}
+                            <p className={styles.vagas}>QUANTIDADE DE VAGAS: <strong>{resultado.Quant_Vagas_Cota}</strong></p>
+                            <p>BÔNUS: <strong>{resultado.Bonus_Percentual} %</strong></p>
+                        </p>
                     </div>
                 )})}
             </div>
--- Frontend/visaosisu/src/components/ResultadoNotas.jsx
+++ Frontend/visaosisu/src/components/ResultadoNotas.jsx
@@ -12,6 +12,20 @@ export default function ResultadoNotas(props){
             <p><strong>{resultado[0]["Nome_Curso"].toUpperCase()}</strong></p>
             <p>{resultado[0]["Grau"].toUpperCase()} - {resultado[0]["Turno"].toUpperCase()}</p>
             <ResultadoCota resultados={resultado}/>
+            <div>
+                <h3>Mais informações:</h3>
+                <p>Nota mínima Matemática: {resultado[0]["Minimo_Nota_MT"]}</p>
+                <p>Peso da nota de Matemática: {resultado[0]["Peso_Nota_MT"]}</p>
+                <p>Nota mínima Ciências da Natureza: {resultado[0]["Minimo_Nota_CN"]}</p>
+                <p>Peso da nota de Ciências da Natureza: {resultado[0]["Peso_Nota_CN"]}</p>
+                <p>Nota mínima Ciências Humanas: {resultado[0]["Minimo_Nota_CH"]}</p>
+                <p>Peso da nota de Ciências Humanas: {resultado[0]["Peso_Nota_CH"]}</p>
+                <p>Nota mínima Linguagens: {resultado[0]["Minimo_Nota_L"]}</p>
+                <p>Peso da nota de Linguagens: {resultado[0]["Peso_Nota_L"]}</p>
+                <p>Nota mínima Redação: {resultado[0]["Minimo_Nota_REDACAO"]}</p>
+                <p>Peso da nota de Redação: {resultado[0]["Peso_Nota_REDACAO"]}</p>
+                <p>Media mínima: {resultado[0]["Media_Minima"]}</p>
+            </div>
         </div>
         
     )

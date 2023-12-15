da844f77685ab1f6432a285da7a94779996d0aed
inaciolimaf <92096585+inaciolimaf@users.noreply.github.com>
2023-12-15 13:52:16 -0300

    FIX: Modificando parênteses confusos

--- Frontend/visaosisu/src/components/ResultadoCota.jsx
+++ Frontend/visaosisu/src/components/ResultadoCota.jsx
@@ -11,20 +11,22 @@ export default function ResultadoCota(props){
                     }
                 }
                 return(
-                    <div className={styles.resultado} key={index}>
-                        <p className={styles.cota}>{resultado.Cota}</p>
-                        <p className={styles.nota}>
-                            <strong className={styles.tituloNota}>NOTAS DE CORTE:</strong>
-                            {notas.map((nota, index)=>{
-                                return (
-                                    <p><span className={styles.notaDia}>{index+1}° DIA: <strong>{nota}</strong></span></p>
-                                )
-                            })}
-                            <p className={styles.vagas}>QUANTIDADE DE VAGAS: <strong>{resultado.Quant_Vagas_Cota}</strong></p>
-                            <p className={styles.bonus}>BÔNUS: * <strong>{resultado.Bonus_Percentual} %</strong></p>
-                        </p>
-                    </div>
-                )})}
+                        <div className={styles.resultado} key={index}>
+                            <p className={styles.cota}>{resultado.Cota}</p>
+                            <p className={styles.nota}>
+                                <strong className={styles.tituloNota}>NOTAS DE CORTE:</strong>
+                                {notas.map((nota, index)=>{
+                                    return (
+                                        <p><span className={styles.notaDia}>{index+1}° DIA: <strong>{nota}</strong></span></p>
+                                    )
+                                })}
+                                <p className={styles.vagas}>QUANTIDADE DE VAGAS: <strong>{resultado.Quant_Vagas_Cota}</strong></p>
+                                <p className={styles.bonus}>BÔNUS: * <strong>{resultado.Bonus_Percentual} %</strong></p>
+                            </p>
+                        </div>)
+                    }
+                )
+            }
                 <p>*: O bônus na nota do ENEM é uma política de cotas que consiste na bonificação adicional percentual de pontos extras à pontuação dos candidatos que se enquadram em critérios específicos.</p>
             </div>
     )
